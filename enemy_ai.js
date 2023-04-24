3434bfe78a358897eefbd808c318a1b8fa63d7f0
Axy-lis <88058208+Axy-lis@users.noreply.github.com>
2023-04-23 20:47:36 -0700

    Create enemy_ai.js

---
+++ enemy_ai.js
@@ -0,0 +1,34 @@
+class Enemy{
+    constructor(){a
+        this.x = random(0, width);
+        this.y = random(0, height);
+        this.playerX = 0;
+        this.playerY = 0;
+        this.velocity = random(4.5, 6);
+
+    } 
+
+    show(){
+        fill(0,0,255);
+        noStroke();
+        rect(this.x, this.y, 40);      
+    }
+
+    follow(playerX, playerY){
+        if(this.x < playerX){
+            this.x += this.velocity;
+        }
+
+        if(this.x > playerX){
+            this.x -= this.velocity;
+        }
+
+        if(this.y < playerY){
+            this.y += this.velocity;
+        }
+
+        if(this.y > playerY){
+            this.y -= this.velocity;
+        }
+    }
+}
